6fbf15ce99eaa6826b8b0dcbac8a896ba9665b89
Ali Maisam <alimaisam@seekasia.com>
2019-03-15 23:59:14 +0800

    remove store

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 'use strict';
+require('dotenv').config()
 var fs = require('fs');
 var path = require('path');
-var configureAWS = require('./src/store');
+var AWS = require('aws-sdk')
+const ddbGeo = require('dynamodb-geo');
 
 exports.get = function(event, context, callback) {
-  configureAWS()
-  .then(function(res) {
+  AWS.config.update({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: "ap-southeast-1",
+  });
+
+  const ddb = new AWS.DynamoDB({ endpoint: new AWS.Endpoint(process.env.ENDPOINT) });
+  const dbconfig = new ddbGeo.GeoDataManagerConfiguration(ddb, process.env.TABLE_NAME);
+  const myGeoTableManager = new ddbGeo.GeoDataManager(dbconfig);
+
+  myGeoTableManager.queryRectangle({
+      MinPoint: {
+          latitude: 2.993943,
+          longitude: 101.52711
+      },
+      MaxPoint: {
+          latitude: 2.994002,
+          longitude: 101.528993
+      }
+  })
+  .then(function(response) {
     var result = {
       statusCode: 200,
-      body: JSON.stringify({message: res}),
+      body: JSON.stringify({message: response}),
       headers: {'content-type': 'application/json'}
     };
-  
+
     callback(null, result);
-  })
-  
+  });
 };
