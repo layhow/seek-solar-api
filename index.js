d303cccafcbfa052b69d72248d9e7feede373ff2
Lay How <layhow.tan@seekasia.com>
2019-03-18 21:59:25 +0800

    update the package

--- index.js
+++ index.js
@@ -1,47 +1,41 @@
-'use strict';
-require('dotenv').config()
-var fs = require('fs');
-var path = require('path');
-var AWS = require('aws-sdk')
 const ddbGeo = require('dynamodb-geo');
+const AWS = require('aws-sdk');
+
+// Set up AWS
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: 'ap-southeast-1'
+});
 
 exports.get = async (event, context, callback) => {
-  AWS.config.update({
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
-    region: "ap-southeast-1",
-  });
+  // Use a local DB for the example.
+  const ddb = new AWS.DynamoDB(AWS.Config);
 
-  const ddb = new AWS.DynamoDB({ endpoint: new AWS.Endpoint(process.env.ENDPOINT) });
-  const dbconfig = new ddbGeo.GeoDataManagerConfiguration(ddb, process.env.TABLE_NAME);
-  const myGeoTableManager = new ddbGeo.GeoDataManager(dbconfig);
+  // Configuration for a new instance of a GeoDataManager. Each GeoDataManager instance represents a table
+  const config = new ddbGeo.GeoDataManagerConfiguration(ddb, process.env.TABLE_NAME);
+  config.hashKeyLength = 3;
 
-  
-  let coordData = await myGeoTableManager.queryRectangle({
-      MinPoint: {
-          latitude: 2.993943,
-          longitude: 101.52711
-      },
-      MaxPoint: {
-          latitude: 2.994002,
-          longitude: 101.528993
-      }
+  // Instantiate the table manager
+  const capitalsManager = new ddbGeo.GeoDataManager(config);
+
+  var coordData = await capitalsManager.queryRadius({
+    RadiusInMeter: 10000,
+    CenterPoint: {
+      latitude: 2.992481,
+      longitude: 101.524913
+    }
   })
-//   .then(function(response) {
-//     var result = {
-//       statusCode: 200,
-//       body: JSON.stringify({message: response}),
-//       headers: {'content-type': 'application/json'}
-//     };
 
-//     callback(null, result);
-//   });
-  
-      var result = {
-      statusCode: 200,
-      body: JSON.stringify({message: coordData}),
-      headers: {'content-type': 'application/json'}
-    };
-  
-    callback(null, result);
+  console.log(coordData);
+
+
+  // console.log(coordData)
+  var result = {
+    statusCode: 200,
+    body: JSON.stringify({ message: coordData }),
+    //headers: {'content-type': 'application/json'}
+  };
+
+  callback(null, result);
 };
